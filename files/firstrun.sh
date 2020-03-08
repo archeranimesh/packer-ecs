7c2ce7c1203ca3be653db581374446bb4977658c
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2020-03-08 20:00:32 +0530

    added various log configuration

--- files/firstrun.sh
+++ files/firstrun.sh
@@ -29,3 +29,42 @@ cwlogs = cwlogs
 [default]
 region = ${AWS_DEFAULT_REGION}
 EOF
+
+
+# Write AWS Logs config
+sudo tee /etc/awslogs/awslogs.conf << EOF > /dev/null
+[general]
+state_file = /var/lib/awslogs/agent-state
+
+ 
+[/var/log/dmesg]
+file = /var/log/dmesg
+log_group_name = ${STACK_NAME}/ec2/${AUTOSCALING_GROUP}/var/log/dmesg
+log_stream_name = {instance_id}
+
+[/var/log/messages]
+file = /var/log/messages
+log_group_name = ${STACK_NAME}/ec2/${AUTOSCALING_GROUP}/var/log/messages
+log_stream_name = {instance_id}
+datetime_format = %b %d %H:%M:%S
+
+[/var/log/docker]
+file = /var/log/docker
+log_group_name = ${STACK_NAME}/ec2/${AUTOSCALING_GROUP}/var/log/docker
+log_stream_name = {instance_id}
+datetime_format = %Y-%m-%dT%H:%M:%S.%f
+
+[/var/log/ecs/ecs-init.log]
+file = /var/log/ecs/ecs-init.log*
+log_group_name = ${STACK_NAME}/ec2/${AUTOSCALING_GROUP}/var/log/ecs/ecs-init
+log_stream_name = {instance_id}
+datetime_format = %Y-%m-%dT%H:%M:%SZ
+time_zone = UTC
+
+[/var/log/ecs/ecs-agent.log]
+file = /var/log/ecs/ecs-agent.log*
+log_group_name = ${STACK_NAME}/ec2/${AUTOSCALING_GROUP}/var/log/ecs/ecs-agent
+log_stream_name = {instance_id}
+datetime_format = %Y-%m-%dT%H:%M:%SZ
+time_zone = UTC
+EOF
